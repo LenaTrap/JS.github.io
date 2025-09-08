f26c3031328007feb628d14b2043449aeca0438f
LenaTrap_ <lena.gadomska09@gmail.com>
2025-09-08 08:30:09 +0200

    Update Gadomska_zewnetrzny.js

--- Gadomska_zewnetrzny.js
+++ Gadomska_zewnetrzny.js
@@ -1,5 +1,4 @@
-
 document.write("ostatnia modyfikacja strony".fontcolor("yellow").txt.strike().fontsize(7)+"<br>"); 
-
 document.write(document.lastModified.fontcolor("yellow").txt.strike().fontsize(7)); 
-
+document.write();
+document.write();
